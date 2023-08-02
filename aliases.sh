e604fdab1b741e64c6dd4702a725450fc3db4904
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2023-08-02 14:42:38 +1000

    removed a lot of stuff from aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -2,9 +2,7 @@
 # Copy entries below to your shell RC file
 
 alias cls=clear
-alias neofetch="neofetch --sixel ~/Pictures/blueruby.png" #change to whatever image you want
 alias matrix=iamb
-alias code="/usr/bin/code-oss --enable-features=UseOzonePlatform --ozone-platform=wayland"
 
 # PATH
 export PATH=$PATH:$HOME/.local/bin
